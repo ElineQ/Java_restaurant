d3201d6ab71f28339672138aaea649e348921c80
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-13 22:32:31 +0200

    Add files via upload

---
+++ food.js
@@ -0,0 +1,34 @@
+let selectedPizza = ['Salami', 'Tuna', 'Margherita'];
+
+let salami = {
+    ingrediënten: 'Salami, mozzarella, parmazaanse kaas, tomatensaus'
+};
+
+let Tuna = {
+    ingrediënten: 'Tonijn, rode uit, geraspte kaas, tomatensaus'
+};
+
+let Margherita = {
+    ingrediënten: 'gerapte kaas, tomatensaus'
+}
+
+if (person = vegatarian) {
+    console.log("Neem de pizza Margherita");
+} else {
+    console.log("Neem pizza Salami of Tuna");
+}
+
+
+let selectedSushi = ['Sushi_box_1', 'Suhi_box_2', 'Sushi_box_3'];
+
+let Sushi_box_1 = {
+    contains: 'nigiri sake, nigiri maguro, nigiri tamago, maki sake, maki maguro'
+};
+
+let Sushi_box_2 = {
+    contains: ' nigiri sake, ebi, maguro, tamago, uramaki, california, maki kappa'
+};
+
+let Sushi_box_3 = {
+    contains: 'uramaki california, nigiri sake, nigiri maguro, plakjes sake, plakjes maguro'
+}
